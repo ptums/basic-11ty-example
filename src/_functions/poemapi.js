060c30dc40d25a6d9c8f0bd303eb798a6596f8d0
ptums <ptumulty923@gmail.com>
2020-12-06 18:39:03 -0500

    removed the id10t error

--- src/_functions/poemapi.js
+++ src/_functions/poemapi.js
@@ -5,7 +5,7 @@ exports.handler = async function(event, context, callback) {
   console.log(term)
 
   const response = await axios.get(`https://poetrydb.org/lines/${term}`)
-  cons
+
   callback(null, {
     statusCode: 200,
     body: response
