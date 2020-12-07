0513a453934db21757a10e8749da125d484b3c6a
ptums <ptumulty923@gmail.com>
2020-12-06 19:31:48 -0500

    taking another step back with initial fetch

--- src/js/poetry.js
+++ src/js/poetry.js
@@ -5,14 +5,16 @@
   console.log('^^ is this formatted properly')
   console.log(`%c request url: /.netlify/functions/poemapi?term=${term.trim()}`,  'color: red; font-size: 32px;')
   fetch(`/.netlify/functions/poemapi?term=${term.trim()}`)
-  .then(response => response.json())
-  .then(data => {
-    const poemElm = document.getElementById('poem')
-    console.log(data)
-    // if(data) {
-    //   poemElm.innerHTML = data
-    // }else {
-    //   poemElm.innerHTML = `Sorry, there aren't any poems about ${term}.`
-    // }
-  });
+  .then(response => {
+    console.log(response)
+  })
+  // .then(data => {
+  //   const poemElm = document.getElementById('poem')
+  //   console.log(data)
+  //   // if(data) {
+  //   //   poemElm.innerHTML = data
+  //   // }else {
+  //   //   poemElm.innerHTML = `Sorry, there aren't any poems about ${term}.`
+  //   // }
+  // });
 })()
