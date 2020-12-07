bee6fa632a9f4096028691d1c54ddb17618e80fd
ptums <ptumulty923@gmail.com>
2020-12-06 19:27:27 -0500

    adding some test messages:

--- src/js/poetry.js
+++ src/js/poetry.js
@@ -1,6 +1,8 @@
 (function() {
   
   const term = location.pathname.split('/').filter(a => a !== "")[0]
+  console.log(`%c term: ${term}`, 'color: green; font-size: 22px;')
+  console.log('^^ is this formatted properly')
   fetch(`/.netlify/functions/poemapi?term=${term}`)
   .then(response => response.json())
   .then(data => {
