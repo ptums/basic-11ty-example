71334aa7a69062b99b9e191603ccd54c36407f70
ptums <ptumulty923@gmail.com>
2020-12-07 08:59:29 -0500

    refactored api call

--- src/_functions/poemapi.js
+++ src/_functions/poemapi.js
@@ -1,15 +1,23 @@
 const axios = require('axios')
 
-exports.handler = async function(event, context, callback) {
-  const term = event.queryStringParameters.term
-  const response = await axios.get(`https://poetrydb.org/lines/${term}`)
+exports.handler = async function(event) {
+  try {
+    const term = event.queryStringParameters.term
+    const response = await axios.get(`https://poetrydb.org/lines/${term}`)
 
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      data: response.data[0].lines.join(),
-      msg: "Here's a poem.",
-      status: 200
-    })
-  });
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        data: response.data[0].lines.join(),
+        msg: "Here's a poem.",
+        status: 200
+      })
+    }
+  } catch(err) {
+    console.error(err)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({err: 'Something went wrong!'})
+    }
+  }
 }
