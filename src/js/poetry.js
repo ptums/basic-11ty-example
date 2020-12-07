b26ec139499197ebb10042ec0975015d8e5f429b
ptums <ptumulty923@gmail.com>
2020-12-07 08:49:27 -0500

    added a new console testing message

--- src/js/poetry.js
+++ src/js/poetry.js
@@ -1,12 +1,13 @@
 (function() {
   
   const term = location.pathname.split('/').filter(a => a !== "")[0]
-  fetch(`https://rachelandmarssite.netlify.app/.netlify/functions/poemapi?term=${term.trim()}`)
+  fetch(`/.netlify/functions/poemapi?term=${term.trim()}`)
   .then(response => {
     return response.json()
   })
-  .then(data => {
-    console.log(`%c data: ${JSON.stringify(data)}`,'color: orange; font-size: 32px')
+  .then(data => {    
+    console.log(`%c response`,'color: orange; font-size: 32px')
+    console.log(data)
   })
   // .then(data => {
   //   const poemElm = document.getElementById('poem')
